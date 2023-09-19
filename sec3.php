bff902c4420c304ca60326c0619bcc51497888db
KingMondz <112163281+KingMondz@users.noreply.github.com>
2023-09-19 09:52:48 +0300

    Section 3 Framework

---
+++ sec3.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bitlink</title>
+</head>
+<body>
+    <div class="why">
+        <div class="2cards1">
+            <div class="card1">
+
+            </div>
+            <div class="card2">
+
+            </div>
+        </div>
+        <div class="2cards2">
+            <div class="card3">
+
+            </div>
+            <div class="card4">
+
+            </div>
+        </div>
+        <div class="whyStatements">
+            <div class="whySmall">
+
+            </div>
+            <div class="whyBig">
+
+            </div>
+            <div class="shortDesc1">
+
+            </div>
+            <div class="shortDesc2">
+
+            </div>
+            <div class="painPoints">
+                <div class="pp1"></div>
+                <div class="pp2"></div>
+                <div class="pp3"></div>
+                <div class="pp4"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
