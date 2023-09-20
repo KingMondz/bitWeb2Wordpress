f15924a2f182061b0ebcd24a2a7dfbf42d89ca84
KingMondz <112163281+KingMondz@users.noreply.github.com>
2023-09-20 16:17:30 +0300

    almost finished sec 3

--- sec3.php
+++ sec3.php
@@ -1,19 +1,27 @@
 <div class="secWhy">   
     <div class="why">
         <div class="cards1">
-            <div class="card1">
-
+            <div class="card1 crd">
+                <i class="fa fa-users" aria-hidden="true"></i>
+                <h4>Solid Team</h4>
+                <p>Our team is professional, well trained and current on new technologies.</p>
             </div>
-            <div class="card2">
-
+            <div class="card2 crd">
+            <i class="fa fa-line-chart" aria-hidden="true"></i>
+                <h4>Kaizen</h4>
+                <p>Continous learning. We acknowledge our missteps and learn from our mistakes.</p>
             </div>
         </div>
         <div class="cards2">
-            <div class="card3">
-
+            <div class="card3 crd">
+            <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                <h4>Experience</h4>
+                <p>With over a decade of experience, Bitlink is your trusted and reliable partner.</p>
             </div>
-            <div class="card4">
-
+            <div class="card4 crd">
+            <i class="fa fa-handshake-o" aria-hidden="true"></i>
+                <h4>Strong Partnerships</h4>
+                <p>Bitlink has strategic partnerships with renowned technology companies</p>
             </div>
         </div>
         <div class="whyStatements">
@@ -41,15 +49,14 @@
                 </ul>
             </div>
             <div class="whyActions">
-                <a href="#">
-                    <div class="learnBtn">
-                        <button>Learn More</button>
+                <a href="#" class="learnBtn">
+                    <div>
+                        Learn More
                     </div>
                 </a>
-                <a href="#">
-                    <div class="watchBtn">
-                        <i class="fa fa-play-circle-o" aria-hidden="true"></i>               
-                        <p>watch video</p>
+                <a href="#" class="watchBtn">
+                    <div>
+                        <i class="fa fa-play-circle-o" aria-hidden="true"></i>watch video
                     </div>
                 </a>
             </div>
